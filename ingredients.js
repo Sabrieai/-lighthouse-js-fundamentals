7ce70228fdba1d1d5850685d03eed4e635ad77ab
Aisha Sabrie <sabriealt@gmail.com>
2021-11-12 14:39:26 -0500

    Iterating Over Arrays: Banana Bread challenge

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// This is the while loop
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+//This is the for loop
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+//This is the reverse loop
+
+for (let i = 7;i-1; i--) {
+  console.log(ingredients[i]);
+}
